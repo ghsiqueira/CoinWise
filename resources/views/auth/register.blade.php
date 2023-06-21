59a924234d280a60d2c2a9a5a247e85978695d7d
fkenjiyunoki <f.kenjiyunoki@gmail.com>
2023-06-20 22:32:51 -0300

    atualização tela de registro

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -15,18 +15,28 @@
             </div>
 
             <div class="mt-4">
+                <x-label for="dateBirth" value="{{ __('Data de Nascimento') }}" />
+                <x-input id="dateBirth" class="block mt-1 w-full" type="date" name="dateBirth" required autofocus autocomplete="dateBirth" />
+            </div>
+
+            <div class="mt-4">
+                <x-label for="phone" value="{{ __('Telefone') }}" />
+                <x-input id="phone" class="block mt-1 w-full" type="text" name="phone" required autofocus autocomplete="phone" />
+            </div>
+
+            <div class="mt-4">
                 <x-label for="email" value="{{ __('Email') }}" />
                 <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
             </div>
 
             <div class="mt-4">
-                <x-label for="password" value="{{ __('Password') }}" />
+                <x-label for="password" value="{{ __('Senha') }}" />
                 <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
             </div>
 
             <div class="mt-4">
-                <x-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-                <x-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
+                <x-label for="password_confirmation" value="{{ __('Confirmar Senha') }}" />
+                <x-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autofocus autocomplete="new-password" />
             </div>
 
             @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
@@ -48,11 +58,11 @@
 
             <div class="flex items-center justify-end mt-4">
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
+                    {{ __('Já possuo uma conta?') }}
                 </a>
 
                 <x-button class="ml-4">
-                    {{ __('Register') }}
+                    {{ __('Registrar') }}
                 </x-button>
             </div>
         </form>
